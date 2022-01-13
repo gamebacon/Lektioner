0a43943ea1f5ea6ba2c86edeb491e05f1475fc65
William With <baconandminecraft@gmail.com>
2022-01-13 15:29:56 +0100

    more stuff

---
+++ ai/python/inlämningsuppgifter/bankapp/src/DataSource.py
@@ -0,0 +1,21 @@
+
+
+class DataSource:
+
+	def datasource_conn(self):
+		
+
+	def get_all(self):
+		#get all customers.
+
+	def update_by_id(self):
+		#update customer
+		return -1;
+
+	def find_by_id(self):
+		#find customer
+		return -1;
+
+	def remove_by_id(self):
+		#remove customer
+		return -1;
