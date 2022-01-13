c6a5189d0919b22e9c642c7e83ea9bc0eb2ccb2e
William With <baconandminecraft@gmail.com>
2022-01-13 13:00:38 +0100

    added bankapp

---
+++ ai/python/inlämningsuppgifter/bankapp/src/Account.py
@@ -0,0 +1,9 @@
+
+class Account:
+	
+	def _init_(self, balance, account_type, account_number):
+		self.balance = balance
+		self.account_type = account_type
+		self.account_number = account_number
+
+
---
+++ ai/python/inlämningsuppgifter/bankapp/src/Customer.py
@@ -0,0 +1,6 @@
+
+
+class Customer:
+	
+	def _init_(self, id, name, person_number, accounts, transactions):
+		
